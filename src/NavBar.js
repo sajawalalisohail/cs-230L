1403e1485c7f134c155ce0a6f193b4838a5fe1eb
Sajawal Ali Sohail <ss0302@mix.wvu.edu>
2024-03-03 21:45:23 -0500

    Navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
 <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">NavBar</a>
+    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
